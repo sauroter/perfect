e0105cc1d28a7a606e34c40d98a44644a6095dd0
s-gheldd <l-gheldd@ti123-ws07.fh-landshut.de>
2015-03-19 11:20:40 +0100

    Funktioniert

--- perfect.go
+++ perfect.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"log"
 	"runtime"
-	"strconv"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	strconv.ParseInt(os.Args[1], 10, 32)
+	//max, err := strconv.ParseInt(os.Args[1], 10, 32)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	max := 200000
+	maker(int(max))
 
 }
 
@@ -17,12 +22,32 @@ func maker(max int) {
 	perfect := make(chan int, numWorkers)
 	numbers := make(chan int, max)
 	done := make(chan struct{}, numWorkers)
-	for i := 2; i <= max; i++ {
+	for i := 1; i <= max; i++ {
 		numbers <- i
 	}
 	close(numbers)
 	for i := 0; i < numWorkers; i++ {
+		log.Println("Worker ", i)
 		go worker(numbers, perfect, done)
+	}
+	numDone := 0
+	for {
+		select {
+
+		case number := <-perfect:
+			{
+				fmt.Println(number)
+			}
+		case <-done:
+			{
+				log.Println("Worker ", numDone, " done")
+				numDone += 1
+				if numDone == numWorkers {
+					return
+				}
+			}
+
+		}
 	}
 
 }
@@ -31,16 +56,19 @@ func worker(numbers, perfect chan int, done chan struct{}) {
 	for num := range numbers {
 
 		sum := 0
-		for i := 0; i <= num; i++ {
+		for i := 1; i < num; i++ {
 
 			if (num % i) == 0 {
-				sum += i
+				sum = sum + i
 			}
+
 		}
+		//fmt.Println(num)
+		//fmt.Println(sum)
 		if sum == num {
 			perfect <- num
 		}
 
 	}
-
+	done <- struct{}{}
 }
